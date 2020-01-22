268b0df58584a5c61a46330038db5e621b974a99
Wenderson <wps713@gmail.com>
2020-01-22 16:30:21 -0300

    Add password encryption test

--- __tests__/integration/Controllers/User.test.js
+++ __tests__/integration/Controllers/User.test.js
@@ -1,4 +1,5 @@
 import request from 'supertest';
+import bcrypt from 'bcryptjs';
 import app from '../../../src/app';
 
 import truncate from '../../util/truncate';
@@ -15,10 +16,21 @@ describe('User', () => {
       .send({
         name: 'Wenderson Pac',
         email: 'wps713@gmail.com',
-        password_hash: '123456',
+        password: '123456',
       });
 
     expect(response.body).toHaveProperty('id');
+  });
+
+  it('should encrypt user password when new user created', async () => {
+    const user = await User.create({
+      name: 'Wenderson Pac',
+      email: 'wps713@gmail.com',
+      password: '123456',
+    });
+    const compareHash = await bcrypt.compare('123456', user.password_hash);
+
+    expect(compareHash).toBe(true);
   });
 
   it('should not be able to register with duplicated email', async () => {
@@ -27,7 +39,7 @@ describe('User', () => {
       .send({
         name: 'Wenderson Pac',
         email: 'wps713@gmail.com',
-        password_hash: '123456',
+        password: '123456',
       });
 
     const response = await request(app)
@@ -35,7 +47,7 @@ describe('User', () => {
       .send({
         name: 'Wenderson Pac',
         email: 'wps713@gmail.com',
-        password_hash: '123456',
+        password: '123456',
       });
     expect(response.status).toBe(400);
   });
